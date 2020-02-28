1e0006b488f63e5e8ce8b6be69fccedd427e450a
Italo Russan <italo.russan@csktech.com.br>
2020-02-28 14:26:29 -0300

    feat: navegar entre paginas

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -3,28 +3,50 @@ import api from '../../services/api';
 
 export default class Main extends Component {
     state = {
-        products: []
-    }
+        products: [],
+        productInfo: {},
+        page: 1
+    };
+
     componentDidMount() {
         this.loadProducts();
     }
 
-    loadProducts = async () => {
-        const response = await api.get('/products');
-        this.setState({products: response.data.docs});
+    loadProducts = async (page = 1) => {
+        const response = await api.get(`/products?page=${page}`);
+        const {docs, ...productInfo} = response.data;
+        this.setState({products: docs, productInfo, page});
+    }
+
+    prevPage = () => {
+        const {page} = this.state;
+        if (page === 1) return;
+        const pageNumber = page - 1;
+        this.loadProducts(pageNumber);
+    }
+
+    nextPage = () => {
+        const {page, productInfo} = this.state;
+        if (page === productInfo.pages) return;
+        const pageNumber = page + 1;
+        this.loadProducts(pageNumber);
     }
 
     render() {
-        const {products} = this.state;
+        const {products, page, productInfo} = this.state;
         return (
             <ul className="product-list">
                 {products.map(product => (
                     <li key={product._id}>
                         <h2>{product.title}</h2>
                         <h4>{product.description}</h4>
-                        <a href={product.url} target="_blank"  rel="noopener noreferrer">Acessar</a>
+                        <a href={product.url} target="_blank" rel="noopener noreferrer">Acessar</a>
                     </li>
                 ))}
+                <div className="pagination">
+                    <button disabled={page === 1} onClick={this.prevPage} id="prev">Anterior</button>
+                    <button disabled={page === productInfo.pages} onClick={this.nextPage} id="next">Próximo</button>
+                </div>
             </ul>
         );
     }
