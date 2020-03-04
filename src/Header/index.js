5c37b1be21bf61a8a0640e686849b819e18d2b64
Italo Russan <italo.russan@csktech.com.br>
2020-03-04 09:29:56 -0300

    fix: titulo do header

--- src/Header/index.js
+++ src/Header/index.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Header =  () => (
     <header className="p-5">
-        <h1 className="display-2">Header ReactJS</h1>
+        <h1 className="display-2">React Shop</h1>
     </header>
 );
 
